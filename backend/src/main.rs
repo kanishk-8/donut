952b85c6468ddfc53366bbf7402ab1b6d8555d82
kanishk-8 <kanishkkumar222004@gmail.com>
2026-01-19 23:22:31 +0530

    fixed the layout for the backend server, will be using this from now on.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+mod core;
+#[tokio::main]
+async fn main() {
+    const PORT: u16 = 8000;
+    println!("Starting server on port {}", PORT);
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", PORT))
+        .await
+        .unwrap();
+    println!("The server is running at port: {PORT}");
+    axum::serve(listener, api::routes()).await.unwrap();
 }
